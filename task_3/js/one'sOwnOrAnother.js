f4f8f977160c95bc94b0929c2eaa6d153a968e6b
Сергей Бельский <belskiysergey90@gmail.com>
2022-06-25 18:46:10 +0300

    fixed condition

--- task_3/js/one'sOwnOrAnother.js
+++ task_3/js/one'sOwnOrAnother.js
@@ -1,8 +1,8 @@
 function isLannisterSoldier(color, lion) {
 
-  return ((color === "red") && (lion === "lion")) ? true : false;
+  return ((color === "red") || (lion === "lion")) ? true : false;
 
 }
 
-console.log(isLannisterSoldier("red", "lion")); // true - чужой - Ланистер
-console.log(isLannisterSoldier("blue", null)); // false - свой - союзник
+console.log(isLannisterSoldier("red", "lion")); // true - Ланистер
+console.log(isLannisterSoldier("blue", null)); // false - другие солдаты
